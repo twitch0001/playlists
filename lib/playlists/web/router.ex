fd1498668baece3d5cfa3b1f9ea12e07c57dba35
twitch <rocharod7@gmail.com>
2020-07-30 22:29:54 +0100

    Random asked

--- lib/playlists/web/router.ex
+++ lib/playlists/web/router.ex
@@ -2,14 +2,21 @@ defmodule Playlists.API.Router do
   use Plug.Router
   
   plug Plug.Logger
+
   plug :match
+  plug Plug.Parsers, 
+    parsers: [:json],
+    pass: ["application/json"],
+    json_decoder: Jason
   plug :dispatch
 
   get "/" do
     send_resp(conn, 200, "Music api")
   end
 
-  forward "/playlists"
+  get "/playlists/:id" do
+    send_resp(conn, 200, "#{id}")
+  end
 
   match _ do 
     send_resp(conn, 404, "Oops! Nothing here.")
--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule Playlists.MixProject do
 
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:jason, "~> 1.2"}
     ]
   end
 end
